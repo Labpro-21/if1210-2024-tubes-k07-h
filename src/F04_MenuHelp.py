dac07ee76c104adec74f1124e2a62203f4dc6816
Diyah Susan N <19623047@std.stei.itb.ac.id>
2024-05-16 14:39:38 +0700

    feat - feat MenuHelp

--- src/F04_MenuHelp.py
+++ src/F04_MenuHelp.py
@@ -1,15 +1,14 @@
-#inisiasi keadaan
 
-def help(status, role):
+def help(status, role, username):
     print ('========== HELP ==========')
     print ()
-    if status == True:
+    if status == True: #kalau sudah login
         if role == "Agent":
-            print ("Halo Agent Purry. Kamu memanggil command HELP. Kamu memilih jalan yang benar, semoga kamu")
+            print (f"Halo Agent {username}. Kamu memanggil command HELP. Kamu memilih jalan yang benar, semoga kamu")
             print ("tidak tersesat kemudian. Berikut adalah hal-hal yang dapat kamu lakukan sekarang:")
             print ()
             print ("    1. Logout\t: Keluar dari akun yang sedang digunakan")
-            print ("    2. Inventory\t: Melihat owca-dex yang dimiliki oleh Agent")
+            print ("    2. Inventory: Melihat owca-dex yang dimiliki oleh Agent")
             print ("    3. Battle\t: Bertarung melawan monster secara random.")
             print ("    4. Arena\t: Arena berlatih untuk meningkatkan kemampuan agen dan para monster.")
             print ("    5. Shop\t: Tempat agen membeli monster dan potion")
@@ -19,7 +18,6 @@ def help(status, role):
             print ("Footnote:")
             print ("    1. Untuk menggunakan aplikasi, silahkan masukkan nama fungsi yang terdaftar")
             print ("    2. Jangan lupa untuk memasukkan input yang valid")
-            cmd = input(">>> ")
 
         elif role == "Admin":
             print ("Selamat datang, Admin. Berikut adalah hal-hal yang dapat kamu lakukan:")
@@ -31,9 +29,8 @@ def help(status, role):
             print ("Footnote:")
             print ("    1. Untuk menggunakan aplikasi, silahkan masukkan nama fungsi yang terdaftar")
             print ("    2. Jangan lupa untuk memasukkan input yang valid")
-            cmd = input(">>> ")
-
-    else:
+            
+    else: #kalau belum login
         print ("Kamu belum login sebagai role apapun. Silahkan login terlebih dahulu.")
         print ()
         print ("    1. Login\t: Masuk ke dalam akun yang sudah terdaftar")
@@ -42,4 +39,4 @@ def help(status, role):
         print ("Footnote:")
         print ("    1. Untuk menggunakan aplikasi, silahkan masukkan nama fungsi yang terdaftar")
         print ("    2. Jangan lupa untuk memasukkan input yang valid")
-        cmd = input(">>> ")
+        
