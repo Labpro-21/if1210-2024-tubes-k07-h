cff8aeaa8b99cb11c836b9db0f96de9f70a02fd1
Diyah Susan N <19623047@std.stei.itb.ac.id>
2024-05-19 14:06:19 +0700

    fix: fix bug monster management

--- src/F13_MonsterManagement.py
+++ src/F13_MonsterManagement.py
@@ -1,6 +1,6 @@
 ###################F13#################################
 # Fungsi untuk memvalidasi sebuah variabel Integer atau bukan
-def is_integer(s):
+def is_integer(s:int):
     try:
         int(s)
         return True
@@ -8,17 +8,18 @@ def is_integer(s):
         return False
 
 # Fungsi untuk menampilkan monster pada file monster.csv
-def tampilkan_monster(monster):
-    print("ID\t|Type\t\t|ATK Power\t|DEF Power\t|HP\t|Stock\t|Price")
+def tampilkan_monster(monster:str):
+    print("ID\t|Type\t\t|ATK Power\t|DEF Power\t|HP")
     for i in range (1, len(monster)):
         print(f"{monster[i][0]}\t|{monster[i][1]}\t\t|{monster[i][2]}\t\t|{monster[i][3]}\t\t|{monster[i][4]}")
     print()
 
 # Fungsi untuk menambahkan monster
-def tambah_monster(monster):
+def tambah_monster(monster:str):
+    print("==============================")
     print("Memulai pembuatan monster baru")
 
-    new_type = input(">>> Masukkan Type / Nam ")
+    new_type = input(">>> Masukkan Type / Nama: ")
 
     # Memvalidasi apakah type monster sudah pernah digunakan sebelumnya atau belum
     existing_type = []
@@ -73,7 +74,7 @@ def tambah_monster(monster):
     print(f"Def Power : {new_def_power}")
     print(f"HP : {new_hp}")
 
-    konfirmasi = str(input(">>> Tambahkan Monster ke database (Y/N) : "))
+    konfirmasi = input(">>> Tambahkan Monster ke database (Y/N) : ")
 
     # Selama jawaban bukan Y atau N, user akan diminta untuk menginput ulang
     while konfirmasi != "Y" and konfirmasi != "N":
@@ -89,46 +90,56 @@ def tambah_monster(monster):
     # Jika jawabannya N (No), monster tidak akan ditambahkan ke file monster.csv
     elif konfirmasi == "N":
         print("Monster gagal ditambahkan!")
+    return monster
 
 # Fungsi utama monster management
-def monster_management(monster):
-    print("")
-    print("█▀ █▀▀ █░░ ▄▀█ █▀▄▀█ ▄▀█ ▀█▀    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄░█ █▀▀    █▀▄ █    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄▄ ▄▀█ █▀ █▀▀ ")
-    print("▄█ ██▄ █▄▄ █▀█ █░▀░█ █▀█ ░█░    █▄▀ █▀█ ░█░ █▀█ █░▀█ █▄█    █▄▀ █    █▄▀ █▀█ ░█░ █▀█ █▄█ █▀█ ▄█ ██▄ ")
-    print("")
-    print("█▀█ ▄▀█ █▀█ ▄▀█   █▀▄▀█ █▀█ █▄░█ █▀ ▀█▀ █▀▀ █▀█   █ █ █")
-    print("█▀▀ █▀█ █▀▄ █▀█   █░▀░█ █▄█ █░▀█ ▄█ ░█░ ██▄ █▀▄   ▄ ▄ ▄")
-    print("")
-    print("")
-    while True:
-        print("1. Tampilkan semua Monster")
-        print("2. Tambah Monster baru")
+def monster_management(role:str, monster:str):
+    if role == "Admin":
+        print("")
+        print("█▀ █▀▀ █░░ ▄▀█ █▀▄▀█ ▄▀█ ▀█▀    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄░█ █▀▀    █▀▄ █    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄▄ ▄▀█ █▀ █▀▀ ")
+        print("▄█ ██▄ █▄▄ █▀█ █░▀░█ █▀█ ░█░    █▄▀ █▀█ ░█░ █▀█ █░▀█ █▄█    █▄▀ █    █▄▀ █▀█ ░█░ █▀█ █▄█ █▀█ ▄█ ██▄ ")
+        print("")
+        print("█▀█ ▄▀█ █▀█ ▄▀█   █▀▄▀█ █▀█ █▄░█ █▀ ▀█▀ █▀▀ █▀█   █ █ █")
+        print("█▀▀ █▀█ █▀▄ █▀█   █░▀░█ █▄█ █░▀█ ▄█ ░█░ ██▄ █▀▄   ▄ ▄ ▄")
+        print("")
+        print("")
+        quit = False
+        while quit == False:
+            print("1. Tampilkan semua Monster")
+            print("2. Tambah Monster baru")
+            print("3. Keluar")
 
-        n = int(input('>>> Pilih Aksi : '))
+            n = input('>>> Pilih Aksi : ')
 
-        # Jika memilih aksi 1, maka list monster akan ditampilkan
-        if n == 1:
-            tampilkan_monster(monster)
-            print() 
-            print("█▀ █▀▀ █░░ ▄▀█ █▀▄▀█ ▄▀█ ▀█▀    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄░█ █▀▀    █▀▄ █    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄▄ ▄▀█ █▀ █▀▀ ")
-            print("▄█ ██▄ █▄▄ █▀█ █░▀░█ █▀█ ░█░    █▄▀ █▀█ ░█░ █▀█ █░▀█ █▄█    █▄▀ █    █▄▀ █▀█ ░█░ █▀█ █▄█ █▀█ ▄█ ██▄ ")
-            print("")
-            print("█▀█ ▄▀█ █▀█ ▄▀█   █▀▄▀█ █▀█ █▄░█ █▀ ▀█▀ █▀▀ █▀█   █ █ █")
-            print("█▀▀ █▀█ █▀▄ █▀█   █░▀░█ █▄█ █░▀█ ▄█ ░█░ ██▄ █▀▄   ▄ ▄ ▄")
-            print("")
-        
-        # Jika memilih aksi 2, maka akan menuju fungsi tambah_monster
-        elif n == 2:
-            tambah_monster(monster)
-            print() 
-            print("█▀ █▀▀ █░░ ▄▀█ █▀▄▀█ ▄▀█ ▀█▀    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄░█ █▀▀    █▀▄ █    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄▄ ▄▀█ █▀ █▀▀ ")
-            print("▄█ ██▄ █▄▄ █▀█ █░▀░█ █▀█ ░█░    █▄▀ █▀█ ░█░ █▀█ █░▀█ █▄█    █▄▀ █    █▄▀ █▀█ ░█░ █▀█ █▄█ █▀█ ▄█ ██▄ ")
-            print("")
-            print("█▀█ ▄▀█ █▀█ ▄▀█   █▀▄▀█ █▀█ █▄░█ █▀ ▀█▀ █▀▀ █▀█   █ █ █")
-            print("█▀▀ █▀█ █▀▄ █▀█   █░▀░█ █▄█ █░▀█ ▄█ ░█░ ██▄ █▀▄   ▄ ▄ ▄")
-            print("")
+            # Jika memilih aksi 1, maka list monster akan ditampilkan
+            if n == '1':
+                tampilkan_monster(monster)
+                print() 
+                print("█▀ █▀▀ █░░ ▄▀█ █▀▄▀█ ▄▀█ ▀█▀    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄░█ █▀▀    █▀▄ █    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄▄ ▄▀█ █▀ █▀▀ ")
+                print("▄█ ██▄ █▄▄ █▀█ █░▀░█ █▀█ ░█░    █▄▀ █▀█ ░█░ █▀█ █░▀█ █▄█    █▄▀ █    █▄▀ █▀█ ░█░ █▀█ █▄█ █▀█ ▄█ ██▄ ")
+                print("")
+                print("█▀█ ▄▀█ █▀█ ▄▀█   █▀▄▀█ █▀█ █▄░█ █▀ ▀█▀ █▀▀ █▀█   █ █ █")
+                print("█▀▀ █▀█ █▀▄ █▀█   █░▀░█ █▄█ █░▀█ ▄█ ░█░ ██▄ █▀▄   ▄ ▄ ▄")
+                print("")
+            
+            # Jika memilih aksi 2, maka akan menuju fungsi tambah_monster
+            elif n == '2':
+                tambah_monster(monster)
+                print() 
+                print("█▀ █▀▀ █░░ ▄▀█ █▀▄▀█ ▄▀█ ▀█▀    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄░█ █▀▀    █▀▄ █    █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄▄ ▄▀█ █▀ █▀▀ ")
+                print("▄█ ██▄ █▄▄ █▀█ █░▀░█ █▀█ ░█░    █▄▀ █▀█ ░█░ █▀█ █░▀█ █▄█    █▄▀ █    █▄▀ █▀█ ░█░ █▀█ █▄█ █▀█ ▄█ ██▄ ")
+                print("")
+                print("█▀█ ▄▀█ █▀█ ▄▀█   █▀▄▀█ █▀█ █▄░█ █▀ ▀█▀ █▀▀ █▀█   █ █ █")
+                print("█▀▀ █▀█ █▀▄ █▀█   █░▀░█ █▄█ █░▀█ ▄█ ░█░ ██▄ █▀▄   ▄ ▄ ▄")
+                print("")
 
-        # Jika memilih aksi selain 1 dan 2, user akan diminta untuk menginput ulang jawaban yang sesuai dengan opsi
-        else :
-            print("Nomor tidak ada di pilihan, silahkan coba lagi.")
-            print()
+            # Jika memilih aksi selain 1 dan 2, user akan diminta untuk menginput ulang jawaban yang sesuai dengan opsi
+            elif n == '3':
+                quit = True
+                print("Terimakasih telah mengunjungi monster management.")
+            else :
+                print("Nomor tidak ada di pilihan, silahkan coba lagi.")
+                print()
+    else:
+        print("Anda bukan seorang Admin. Anda tidak dapat mengakses monster management.")
+    return monster
