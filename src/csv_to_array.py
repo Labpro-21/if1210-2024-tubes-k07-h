8a1b3827e309211df5e4685646ba13356f927ccc
Diyah Susan N <19623047@std.stei.itb.ac.id>
2024-05-19 12:37:12 +0700

    fix: fix bug

--- src/csv_to_array.py
+++ src/csv_to_array.py
@@ -14,11 +14,10 @@ def csv_separate(line):
 def csv_to_array(folder, file):
     path = os.path.join(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)), 'data', folder, file)
     user_file = open(path, 'r')
-
+    user_file = [raw.replace('\n', '') for raw in user_file]
     arr = []
-    
     for line in user_file:
-        inp_arr = csv_separate(line.replace('n', ''))
+        inp_arr = csv_separate(line)
         arr.append(inp_arr)
     
     return arr
