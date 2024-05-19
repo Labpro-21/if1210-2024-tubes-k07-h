f5071238ce0dcff168b640a1516a927e54c9f548
Diyah Susan N <19623047@std.stei.itb.ac.id>
2024-05-19 18:28:26 +0700

    fix: fix bug login

--- src/F02_Login.py
+++ src/F02_Login.py
@@ -39,7 +39,7 @@ def login(user:str, status:bool):
                     role = user[i][3]
                     user_id = user[i][0]
                     username = user[i][1]
-                    owca = 0
+                    owca = int(user[i][4])
                     return status, user_id, username, role, owca
                     
                 elif i == len(user) -1: #artinya sudah dicek sampai terakhir
